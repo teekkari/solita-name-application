2659c1ed7af6d570b1439b43b7ab7a206bbe6b88
unknown <lanttila98@gmail.com>
2021-01-20 11:45:02 +0200

    created components folder and skeletons for Namelist and Quickstart component classes

---
+++ src/components/Quickstart.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+class Quickstart extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+
+    render() {
+        return null;
+    }
+}
+
+export default Quickstart;
