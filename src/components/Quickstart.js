2ea4adbc3bcefa9ae32a2beb8f5fdc6f71a25916
unknown <lanttila98@gmail.com>
2021-01-20 11:59:22 +0200

    Switched Quickstart comp. to be a function, no need for class.

--- src/components/Quickstart.js
+++ src/components/Quickstart.js
@@ -1,27 +1,22 @@
-import React from 'react';
+/*
+    Renders "Quick Start" panel with instructions for application usage.
+    Styling is in App.css
+*/
 
+function Quickstart() {
+    return (
+    <div className="quickstart">
+        <div className="quickstart-content">
+            <h1>Quickstart</h1>
 
-class Quickstart extends React.Component {
-    constructor(props) {
-        super(props);
-    }
+            <h2>Sorting</h2>
+            <div className="text">By clicking on the labels <b>Name</b> or <b>Amount</b>, you can sort the list in alphabetical or numerical order.</div>
 
-
-    render() {
-        return (
-        <div className="quickstart">
-            <div className="quickstart-content">
-                <h1>Quickstart</h1>
-
-                <h2>Sorting</h2>
-                <div className="text">By clicking on the labels <b>Name</b> or <b>Amount</b>, you can sort the list in alphabetical or numerical order.</div>
-
-                <h2>Searching</h2>
-                <div className="text">You can search a name and its popularity with the search bar on top of the list.</div>
-            </div>
+            <h2>Searching</h2>
+            <div className="text">You can search a name and its popularity with the search bar on top of the list.</div>
         </div>
-        );
-    }
+    </div>
+    );
 }
 
 export default Quickstart;
