15f341816dbb1709de8a716aac780a15853bf701
unknown <lanttila98@gmail.com>
2021-01-20 13:52:24 +0200

    removed unused className and added a space to h1 title

--- src/components/Quickstart.js
+++ src/components/Quickstart.js
@@ -7,14 +7,14 @@ function Quickstart() {
     return (
     <div className="quickstart">
         <div className="quickstart-content">
-            <h1>Quickstart</h1>
+            <h1>Quick start</h1>
 
             <h2>Sorting</h2>
             <p>By clicking on the labels <b>Name</b> or <b>Amount</b>, you can sort the list in alphabetical or numerical order.</p>
             <p>Clicking again on the active sorting label changes order between ascending and descending.</p>
 
             <h2>Searching</h2>
-            <p className="text">You can search a name and its popularity with the search bar on top of the list.</p>
+            <p>You can search a name and its popularity with the search bar on top of the list.</p>
         </div>
     </div>
     );
