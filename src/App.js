1b8e239babe3a8c5954c8125c7ea9b20109a785d
unknown <lanttila98@gmail.com>
2021-01-20 11:41:48 +0200

    Added bg color to body and added app title with styling.

--- src/App.js
+++ src/App.js
@@ -3,6 +3,14 @@ import './App.css';
 function App() {
   return (
     <div className="app">
+      <header>
+        <h1 className="app-title">Name application</h1>
+        <div className="app-secondary-title">Solita 2021</div>
+      </header>
+
+      <main>
+        
+      </main>
     </div>
   );
 }
